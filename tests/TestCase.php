63ee803d4e796f00b632de158eab9ca40b8f17b1
omaressaouaf <souafomar@gmail.com>
2025-03-18 17:04:12 +0000

    patch tests.yml ci

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -14,7 +14,7 @@ class TestCase extends \Orchestra\Testbench\TestCase
     {
         parent::setUp();
 
-        $this->loadMigrationsFrom(__DIR__.'/Migrations');
+        $this->loadMigrationsFrom(__DIR__ . '/Migrations');
     }
 
     protected function getPackageProviders($app)
@@ -29,11 +29,12 @@ class TestCase extends \Orchestra\Testbench\TestCase
     {
         config()->set('database.default', 'testing');
         config()->set('database.connections.testing', [
-            'driver'   => 'mysql',
-            'host' => 'localhost',
-            'database' => 'query_builder_criteria_testing',
-            'username' => 'root',
-            'password' => '',
+            'driver'   => env('DB_CONNECTION', 'mysql'),
+            'host' => env('DB_HOST', '127.0.0.1'),
+            'port' => env('DB_PORT', '3306'),
+            'database' => env('DB_DATABASE', 'query_builder_criteria_testing'),
+            'username' => env('DB_USER', 'root'),
+            'password' => env('DB_PASSWORD', ''),
         ]);
     }
 }
